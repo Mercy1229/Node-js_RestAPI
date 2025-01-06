61a0d2b00e8676cdd19d7971a4b2b033dbcdcda7
Mercy1229 <mercysamuel1229@gmail.com>
2025-01-06 18:10:47 +0530

    updated

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required: true,
+        required:true,
     },
 });
 const User = mongoose.model('User', userSchema);
